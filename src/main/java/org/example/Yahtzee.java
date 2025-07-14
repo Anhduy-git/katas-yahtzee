cfbde25a471a0afaedf781e0c1e3da43f6af3a12
Tran Anh Duy NGUYEN <anhduydp123@gmail.com>
2025-07-15 00:05:59 +0200

    feat: add score calculation for basic categories

--- src/main/java/org/example/Yahtzee.java
+++ src/main/java/org/example/Yahtzee.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Yahtzee {
     public static int calculateScore(int[] dices, String category) {
-        return 0;
+        return switch (category) {
+            case "Ones" -> Arrays.stream(dices).filter((dice) -> dice == 1).sum();
+            case "Twos" -> Arrays.stream(dices).filter((dice) -> dice == 2).sum();
+            case "Threes" -> Arrays.stream(dices).filter((dice) -> dice == 3).sum();
+            case "Fours" -> Arrays.stream(dices).filter((dice) -> dice == 4).sum();
+            case "Fives" -> Arrays.stream(dices).filter((dice) -> dice == 5).sum();
+            case "Sixes" -> Arrays.stream(dices).filter((dice) -> dice == 6).sum();
+            default -> throw new IllegalStateException("Unexpected value: " + category);
+        };
     }
 }
--- src/test/java/org/example/YahtzeeTest.java
+++ src/test/java/org/example/YahtzeeTest.java
@@ -3,12 +3,42 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-class YahtzeeTest {
 
+class YahtzeeTest {
     @Test
     void calculateScore_withCategoryOnes() {
         int[] dices = new int[]{1, 1, 2, 4, 5};
         int score = Yahtzee.calculateScore(dices, "Ones");
         assertEquals(2, score);
+    }
+    @Test
+    void calculateScore_withCategoryTwos() {
+        int[] dices = new int[]{1, 4, 2, 2, 2};
+        int score = Yahtzee.calculateScore(dices, "Twos");
+        assertEquals(6, score);
+    }
+    @Test
+    void calculateScore_withCategoryThrees() {
+        int[] dices = new int[]{1, 3, 6, 5, 2};
+        int score = Yahtzee.calculateScore(dices, "Threes");
+        assertEquals(3, score);
+    }
+    @Test
+    void calculateScore_withCategoryFours() {
+        int[] dices = new int[]{1, 4, 2, 4, 5};
+        int score = Yahtzee.calculateScore(dices, "Fours");
+        assertEquals(8, score);
+    }
+    @Test
+    void calculateScore_withCategoryFives() {
+        int[] dices = new int[]{5, 4, 5, 2, 5};
+        int score = Yahtzee.calculateScore(dices, "Fives");
+        assertEquals(15, score);
+    }
+    @Test
+    void calculateScore_withCategorySixes() {
+        int[] dices = new int[]{3, 4, 6, 2, 6};
+        int score = Yahtzee.calculateScore(dices, "Sixes");
+        assertEquals(12, score);
     }
 }
