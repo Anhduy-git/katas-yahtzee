087c169aa880a18159265170634f09b994bfe732
Tran Anh Duy NGUYEN <anhduydp123@gmail.com>
2025-07-15 22:37:01 +0200

    feat: refactor code, add Pair and TwoPairs categories

--- src/main/java/org/example/Yahtzee.java
+++ src/main/java/org/example/Yahtzee.java
@@ -1,17 +1,60 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Yahtzee {
     public static int calculateScore(int[] dices, String category) {
         return switch (category) {
-            case "Ones" -> Arrays.stream(dices).filter((dice) -> dice == 1).sum();
-            case "Twos" -> Arrays.stream(dices).filter((dice) -> dice == 2).sum();
-            case "Threes" -> Arrays.stream(dices).filter((dice) -> dice == 3).sum();
-            case "Fours" -> Arrays.stream(dices).filter((dice) -> dice == 4).sum();
-            case "Fives" -> Arrays.stream(dices).filter((dice) -> dice == 5).sum();
-            case "Sixes" -> Arrays.stream(dices).filter((dice) -> dice == 6).sum();
-            default -> throw new IllegalStateException("Unexpected value: " + category);
+            case "Ones", "Twos", "Threes", "Fours", "Fives", "Sixes" -> calculateScoreBasicCategories(dices, category);
+            case "Pair" -> calculateScorePairCategory(dices);
+            case "TwoPairs" -> calculateScoreTwoPairsCategory(dices);
+            default -> throw new IllegalArgumentException("Unexpected value: " + category);
         };
+    }
+
+    public static int calculateScoreBasicCategories(int[] dices, String category) {
+        Map<String, Integer> map = Map.of(
+                "Ones", 1,
+                "Twos", 2,
+                "Threes", 3,
+                "Fours", 4,
+                "Fives", 5,
+                "Sixes", 6
+        );
+        return Arrays.stream(dices).filter((dice) -> dice == map.get(category)).sum();
+    }
+
+    public static int calculateScorePairCategory(int[] dices) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int dice : dices) {
+            countMap.put(dice, countMap.getOrDefault(dice, 0) + 1);
+        }
+        for (int i = 6; i >= 1; i--) {
+            if (countMap.containsKey(i) && countMap.get(i) >= 2) {
+                return i * 2;
+            }
+        }
+        return 0;
+    }
+
+    public static int calculateScoreTwoPairsCategory(int[] dices) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int dice : dices) {
+            countMap.put(dice, countMap.getOrDefault(dice, 0) + 1);
+        }
+        int score = 0;
+        int pairsFound  = 0;
+        for (int i = 6; i >= 1; i--) {
+            if (countMap.containsKey(i) && countMap.get(i) >= 2) {
+                score += i * 2;
+                pairsFound ++;
+                if (pairsFound  == 2) {
+                    return score;
+                }
+            }
+        }
+        return 0;
     }
 }
--- src/test/java/org/example/YahtzeeTest.java
+++ src/test/java/org/example/YahtzeeTest.java
@@ -11,34 +11,60 @@ class YahtzeeTest {
         int score = Yahtzee.calculateScore(dices, "Ones");
         assertEquals(2, score);
     }
+
     @Test
     void calculateScore_withCategoryTwos() {
         int[] dices = new int[]{1, 4, 2, 2, 2};
         int score = Yahtzee.calculateScore(dices, "Twos");
         assertEquals(6, score);
     }
+
     @Test
     void calculateScore_withCategoryThrees() {
         int[] dices = new int[]{1, 3, 6, 5, 2};
         int score = Yahtzee.calculateScore(dices, "Threes");
         assertEquals(3, score);
     }
+
     @Test
     void calculateScore_withCategoryFours() {
         int[] dices = new int[]{1, 4, 2, 4, 5};
         int score = Yahtzee.calculateScore(dices, "Fours");
         assertEquals(8, score);
     }
+
     @Test
     void calculateScore_withCategoryFives() {
         int[] dices = new int[]{5, 4, 5, 2, 5};
         int score = Yahtzee.calculateScore(dices, "Fives");
         assertEquals(15, score);
     }
+
     @Test
     void calculateScore_withCategorySixes() {
         int[] dices = new int[]{3, 4, 6, 2, 6};
         int score = Yahtzee.calculateScore(dices, "Sixes");
         assertEquals(12, score);
+    }
+
+    @Test
+    void calculateScore_withCategoryPair() {
+        int[] dices = new int[]{3, 4, 6, 2, 6};
+        int score = Yahtzee.calculateScore(dices, "Pair");
+        assertEquals(12, score);
+    }
+
+    @Test
+    void calculateScore_withCategoryTwoPairs() {
+        int[] dices = new int[]{1, 1, 2, 2, 3};
+        int score = Yahtzee.calculateScore(dices, "TwoPairs");
+        assertEquals(6, score);
+    }
+
+    @Test
+    void calculateScore_noMatchingCategory() {
+        int[] dices = new int[]{3, 4, 6, 2, 6};
+        Exception e = assertThrows(IllegalArgumentException.class, () -> Yahtzee.calculateScore(dices, "SomeCategory"));
+        assertEquals("Unexpected value: SomeCategory", e.getMessage());
     }
 }
